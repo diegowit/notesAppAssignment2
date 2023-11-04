095ba1695e4896189eb57ddf6e69fd2b0acdb028
diego <20098757@mail.wit.ie>
2023-11-04 19:51:35 +0000

    Variables added

--- src/main/kotlin/models/Note.kt
+++ src/main/kotlin/models/Note.kt
@@ -1,4 +1,8 @@
 package models
 
-class Note {
+
+data class Note (var noteTitle: String,
+                 var notePriority: Int,
+                 var noteCategory: String,
+                 var isNoteArchived: Boolean){
 }
